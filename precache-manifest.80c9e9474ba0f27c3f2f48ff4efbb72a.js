76dae24dd7bdfa2c2bb69cbf5a8f5297f3ea6613
edelBCS <edelm8@gmail.com>
2019-10-31 18:45:17 -0700

    Updates

---
+++ precache-manifest.80c9e9474ba0f27c3f2f48ff4efbb72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147a8beb93ed37539ae03f77e765940d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d871e43e3b09af440887",
+    "url": "/clicky-game/static/css/main.c9d997db.chunk.css"
+  },
+  {
+    "revision": "4784df0b8fd6cc0c2ae9",
+    "url": "/clicky-game/static/js/2.71897b9e.chunk.js"
+  },
+  {
+    "revision": "d871e43e3b09af440887",
+    "url": "/clicky-game/static/js/main.44510c1f.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
